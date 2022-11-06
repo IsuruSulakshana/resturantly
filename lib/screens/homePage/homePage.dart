a2d16256efb6325e8a5a9248bd83cf83b7907899
Isuru Sulakshana <pererawhis.20@uom.lk>
2022-11-07 01:10:57 +0530

    change

--- lib/screens/homePage/homePage.dart
+++ lib/screens/homePage/homePage.dart
@@ -17,7 +17,7 @@ class _HomeState extends State<Home> {
         title: Text(
           "Resturantly",
           style: TextStyle(
-            fontSize: 30,
+            fontSize: 3,
           ),
         ),
       ),
