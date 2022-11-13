b73a77296f1ebd0915a29ca6ffe2fa40eb2acaef
Uditha <63743940+Uditha05@users.noreply.github.com>
2022-11-13 12:19:15 +0530

    add chat page

--- lib/screens/chatPage/components/chat.dart
+++ lib/screens/chatPage/components/chat.dart
@@ -1,6 +1,4 @@
-import 'package:flutter_chat/chatData.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chat/chatWidget.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = "welcome_screen";
@@ -12,14 +10,10 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   void initState() {
     super.initState();
-    ChatData.init("Just Chat",context);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: ChatWidget.getAppBar(ChatData.appName),
-        backgroundColor: Colors.white,
-        body: ChatWidget.widgetWelcomeScreen(context));
+    return Scaffold();
   }
 }
--- lib/screens/chatPage/components/photo.dart
+++ lib/screens/chatPage/components/photo.dart
@@ -3,29 +3,33 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:resturantly/screens/chatPage/chatPage.dart';
+import 'package:resturantly/screens/chatPage/chatPage2.dart';
 
 class Photo extends StatefulWidget {
-  const Photo({Key? key,}) : super(key: key);
+  const Photo({
+    Key? key,
+  }) : super(key: key);
   @override
   State<Photo> createState() => _PhotoState();
 }
-class _PhotoState extends State<Photo> {
 
-  var image;
+class _PhotoState extends State<Photo> {
+  XFile? image;
+  File? file;
   void openCamera() async {
     PickedFile? picked = await ImagePicker().getImage(
         preferredCameraDevice: CameraDevice.front, source: ImageSource.camera);
-      setState(() {
-        image = File(picked!.path);
-      }
-    );
+    setState(() {
+      image = XFile(picked!.path);
+      file = File(picked.path);
+    });
   }
 
-   @override
-    void initState() {
-      super.initState();
-      openCamera();
-    }
+  @override
+  void initState() {
+    super.initState();
+    openCamera();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,62 +41,69 @@ class _PhotoState extends State<Photo> {
         backgroundColor: Colors.black,
         actions: [
           IconButton(
-            onPressed: (){
+            onPressed: () {
               setState(() {
                 openCamera();
-                }
-              );
-            }, 
+              });
+            },
             icon: Icon(
               Icons.close,
-              size: area*(2.0),
+              size: area * (2.0),
               color: Colors.white,
-              ),
             ),
+          ),
         ],
       ),
       body: Center(
-      child: image != null ? Column(
-              children: [
-                Container(
-                  height: screenHeight*0.6,
-                  child: Image.file(image),
-                ),
-                SizedBox(height: screenHeight*0.07,),
-                Container(
-                  width: screenWidth*0.8,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12.0)
+        child: image != null
+            ? Column(
+                children: [
+                  Container(
+                    height: screenHeight * 0.6,
+                    child: Image.file(file!),
                   ),
-                  child: FlatButton(
-                    onPressed: (){
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (BuildContext context) => Chat(image: image,)));
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Start Chatting",
-                          style: TextStyle(
-                            fontSize: area*(19/17),
+                  SizedBox(
+                    height: screenHeight * 0.07,
+                  ),
+                  Container(
+                    width: screenWidth * 0.8,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12.0)),
+                    child: FlatButton(
+                      onPressed: () {
+                        // Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        //     builder: (BuildContext context) => Chat(image: image,)));
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (BuildContext context) => ChatPage2(
+                                  selfieImage: image,
+                                )));
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Start Chatting",
+                            style: TextStyle(
+                              fontSize: area * (19 / 17),
+                            ),
                           ),
-                        ),
-                        SizedBox(width: screenWidth*0.01,),
-                        Icon(
-                          Icons.arrow_forward_outlined,
-                          size: area*(1.4),
-                        ),
-                      ],
+                          SizedBox(
+                            width: screenWidth * 0.01,
+                          ),
+                          Icon(
+                            Icons.arrow_forward_outlined,
+                            size: area * (1.4),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ) : Container(),
-        ),
+                ],
+              )
+            : Container(),
+      ),
       backgroundColor: Colors.black,
     );
   }
 }
-
