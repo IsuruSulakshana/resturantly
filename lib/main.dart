fafbf0080d7e3927dd36d097caa068e622a13326
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-11-18 00:11:51 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:resturantly/widget/chat/photo.dart';
 import 'package:resturantly/screens/homePage/homePage.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() {
-  runApp(
-    const MyApp(),
-  );
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      useInheritedMediaQuery: true,
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        brightness: Brightness.light
-      ),
-      home: const Home(),
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const Home()
+      //home: const WidgetTesting(),
+      //home: const myScreen(),
     );
   }
 }
--- lib/screens/chatPage/chatPage.dart
+++ lib/screens/chatPage/chatPage.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'dart:ffi';
+//import 'dart:ffi';
 import 'dart:io';
 
 // import 'package:file_picker/file_picker.dart';
